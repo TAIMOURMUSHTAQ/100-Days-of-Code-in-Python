a66a4987292a9dc976fa2cd7e14d1e44a237b760
TAIMOUR MUSHTAQ <taimourmushtaq2@gmail.com>
2026-07-26 15:25:28 -0300

    Day 3 completed upto Logical Operator

---
+++ Day 3/If Else/task.py
@@ -0,0 +1,8 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+if height >= 18:
+    print("You can ride the rollercoaster!")
+elif height >= 16:
+    print("You cannot ride the rollercoaster!")
+else :
+    print("Your height is too high!")
---
+++ Day 3/Modulo/task.py
@@ -0,0 +1,8 @@
+print(10%3)
+number=int(input("Enter a number: "))
+if number % 2 == 0:
+    print(f"{number} is even")
+elif number % 2 == 1:
+    print(f"{number} is odd")
+else :
+    print(f"{number} is too high")
