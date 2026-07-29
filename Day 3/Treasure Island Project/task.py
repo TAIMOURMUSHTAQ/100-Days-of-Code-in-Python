9a81e76e8ca2d2f02fb035e5432ce87a7624443d
TAIMOUR MUSHTAQ <taimourmushtaq2@gmail.com>
2026-07-29 15:02:30 -0300

    Updated the project

--- Day 3/Treasure Island Project/task.py
+++ Day 3/Treasure Island Project/task.py
@@ -23,17 +23,19 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 print("You're at a cross road. Where do you want to go?\n\tType \"left\" or \"right\".")
-direction=input()
+direction=input().lower()
 if(direction=="left"):
     print("You've come to a lake. There is an island in the middle of the lake.\n\tType \"wait\" to wait for a boat. Type \"swim\" to swim across.")
-    decision=input()
+    decision=input().lower()
     if(decision=="wait"):
         print("You arrive at the island unharmed. There is a house with 3 doors.One red, one yellow and one blue. Which colour do you choose?")
-        color=input()
+        color=input().lower()
         if color=="red":
-            print("You enterd wrong room.Game Over!")
+            print("It is a room full of fire.Game Over!")
         elif color=="yellow":
             print("You found the treasure.You win!")
+        elif color=="blue":
+            print("You enter a room of beasts.Game Over!")
         else:
             print("Dead.Game Over!")
     else:
