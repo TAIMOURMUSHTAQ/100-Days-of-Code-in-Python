6ce7d9cadd1e73b0f3aa81e89e20c2bb6f5e1e6e
TAIMOUR MUSHTAQ <taimourmushtaq2@gmail.com>
2026-08-06 14:55:05 -0300

    Cease Cipher I

--- Day 8/Caesar Cipher 1/main.py
+++ Day 8/Caesar Cipher 1/main.py
@@ -11,15 +11,28 @@ shift = int(input("Type the shift number:\n"))
 
 
 # TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
-def encrypt(original_text, shift_amount):
-    encrypted_text = ""
-    # for char in original_text:
-
 # TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
 #  by the shift amount and print the encrypted text.
+def encrypt(original_text, shift_amount):
+    cipher_text = ""
+    for letter in original_text:
+        shifted_position=alphabet.index(letter)+shift_amount
+        cipher_text=alphabet[shifted_position]
+    # TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
+    # if(shifted_position>27):
+    #     shifted_position%27
+    shifted_position %= len(alphabet)
+    print(f"Here is the encoded result: {cipher_text}")
 
-# TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
+
 
 # TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
 #  message.
+encrypt(original_text=text, shift_amount=shift)
 
+# def decrypt(original_text, shift_amount):
+#     cipher_text = ""
+#     for letter in original_text:
+#         shifted_position=alphabet.index(letter)+shift_amount
+#         cipher_text=alphabet[shifted_position]
+#         print(f"Here is the decoded result: {cipher_text}")
