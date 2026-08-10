4a18070b378c03cf726193e62d22394444fd0fb8
TAIMOURMUSHTAQ <taimourmushtaq2@gmail.com>
2026-08-10 12:39:30 -0300

    Day 10 Completed

--- Day 10/Docstrings/task.py
+++ Day 10/Docstrings/task.py
@@ -1,4 +1,10 @@
 def format_name(f_name, l_name):
+    """
+    Take a first name and a last name and return a formatted name
+    :param f_name:
+    :param l_name:
+    :return:
+    """
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}"
