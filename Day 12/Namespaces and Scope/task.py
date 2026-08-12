e63295edb334fa85de348d2f1ae553772660fc3c
TAIMOURMUSHTAQ <taimourmushtaq2@gmail.com>
2026-08-12 15:23:50 -0300

    Day 12 Underway...

--- Day 12/Namespaces and Scope/task.py
+++ Day 12/Namespaces and Scope/task.py
@@ -8,3 +8,17 @@ def increase_enemies():
 
 increase_enemies()
 print(f"enemies outside function: {enemies}")
+def game():
+    def drink_portion():
+        portion_strength=10
+        print(portion_strength)
+
+drink_portion() #Function inside another function is local function
+# print(portion_strength)
+
+player_health=10
+def increase_health():
+    player_health=20
+    print(f"player_health inside function: {player_health}")
+    print(f"{enemies} health inside function: {player_health}")
+increase_health()
