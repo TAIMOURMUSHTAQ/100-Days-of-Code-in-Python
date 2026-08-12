c65b040ab23caf9462b8b0edaab6ec6ef7fa2b60
TAIMOURMUSHTAQ <taimourmushtaq2@gmail.com>
2026-08-12 15:50:16 -0300

    Day 12 Underway...

---
+++ Day 12/Global Constants/task.py
@@ -0,0 +1,4 @@
+#Global Constants
+PI=3.14159
+U_R_L="www.Google.com"
+print(U_R_L)
---
+++ Day 12/Number Guessing Project/main.py
@@ -0,0 +1,7 @@
+print("Welcome to the Number Guessing Game!")
+
+print("I'm thinking of a number between 1 and 100.")
+
+difficulty_level=input("Choose a difficulty level.Type 'easy' or 'hard': ")
+
+"if hard you have 5 attemps otherwise 10"
