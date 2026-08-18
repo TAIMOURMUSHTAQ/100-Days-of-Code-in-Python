f19aeba9da6b51db64f76c3b35c71c06da6a75f0
TAIMOURMUSHTAQ <taimourmushtaq2@gmail.com>
2026-08-18 15:29:40 -0300

    Minor changes

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-import turtle
-from turtle import Turtle,Screen
-timmy=Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("cornflowerblue")
-turtle.forward(50)
+# import turtle
+# from turtle import Turtle,Screen
+# timmy=Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("cornflowerblue")
+# turtle.forward(50)
+#
+# my_screen=Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-my_screen=Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+from prettytable import PrettyTable
+# table=prettytable.PrettyTable()
+# print(table)
+table=PrettyTable()
+table.add_column("Pokeymon",["Pikachu" ,"Squirl" ,"Elsiveer"],"l")
+table.add_column("Type",["Animal","Bird","Snail"],"c")
+print(table)
